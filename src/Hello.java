85521720c441f49ed781350e4a36b032d53aa880
kokokaki <hsg9984@gmail.com>
2021-12-27 12:48:59 +0900

    [21.12.27] # static, final

--- src/Hello.java
+++ src/Hello.java
@@ -1,23 +1,18 @@
 
 
-class Car {
 
-    String model;
-
-    public Car(String model) {
-        this.model = model;
-    }
-
-    void accel() {
-        System.out.println(model + " 주행을 시작합니다.");
-    }
-}
 
 public class Hello {
 
+    static int a;
+
+    static void mtd() {
+
+    }
+
     public static void main(String[] args) {
-        Car ferrari = new Car("페라리");
-        ferrari.accel();
+        a = 10;
+        mtd();
     }
 
 }
---
+++ src/day13/protec/pac2/D.java
@@ -0,0 +1,22 @@
+package day13.protec.pac2;
+
+import day13.protec.pac1.A;
+
+public class D extends A {
+
+
+
+    public D() {
+
+//        double pi = Math.PI;
+
+//        super(5.5); //default
+        super(100);
+
+        this.f1 = 1;
+//        this.f2 = 2;
+
+        this.m1();
+//        this.m2();
+    }
+}
