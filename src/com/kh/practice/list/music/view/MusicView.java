166fc16c439736623518e3e664d9366c54ed3309
kokokaki <hsg9984@gmail.com>
2022-01-06 12:49:10 +0900

    [21.01.06] # Set, Map 컬렉션

---
+++ src/com/kh/practice/list/music/view/MusicView.java
@@ -0,0 +1,21 @@
+package com.kh.practice.list.music.view;
+
+import com.kh.practice.list.music.controller.MusicController;
+
+import java.util.Scanner;
+
+public class MusicView {
+
+    private Scanner sc = new Scanner(System.in);
+    private MusicController mc = new MusicController();
+
+    public void mainMenu() {}
+    public void addList() {}
+    public void addAtZero() {}
+    public void printAll() {}
+    public void searchMusic() {}
+    public void removeMusic() {}
+    public void setMusic() {}
+    public void ascTitle() {}
+    public void descSinger() {}
+}
